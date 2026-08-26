nekoshirro/platform_kernel_xiaomi_marble

drivers: qcom: sys_pm_vx: Add waipio support
This change enables sys_pm_vx to work for waipio.

While doing this corrects sys_pm_vx to depends on QCOM_AOSS_QMP
because qcom_aoss provide same debugfs functionality as
qmp-debugfs-client driver.

Change-Id: I34494331f326e7571bea0bb74460a30714ec3894
Signed-off-by: Tushar Nimkar <tnimkar@codeaurora.org>

drivers/soc/qcom/sys_pm_vx.c
@@ -80,6 +80,14 @@ static const char * const drv_names_lahaina[] = {
 	""
 };
 
+static const char * const drv_names_waipio[] = {
+	"TZ", "HYP", "HLOS", "L3", "SECPROC", "AUDIO", "SENSOR", "AOP",
+	"DEBUG", "GPU", "DISPLAY", "COMPUTE_DSP", "TIME_SW", "TIME_HW",
+	"MDM SW", "MDM HW", "WLAN RF", "WLAN BB", "DDR AUX", "ARC CPRF",
+	"ARC_DEP", "BCM",
+	""
+};
+
 static int read_vx_data(struct vx_platform_data *pd, struct vx_log *log)
 {
 	void __iomem *base = pd->base;
@@ -232,6 +240,8 @@ static int vx_create_debug_nodes(struct vx_platform_data *pd)
 static const struct of_device_id drv_match_table[] = {
 	{ .compatible = "qcom,sys-pm-lahaina",
 	  .data = drv_names_lahaina },
+	{ .compatible = "qcom,sys-pm-waipio",
+	  .data = drv_names_waipio },
 	{ }
 };
 

LineageOS/android_kernel_qcom_sm8750

drivers/soc/qcom/sys_pm_vx.c
@@ -107,6 +107,13 @@ enum {
 	AOSS_SHUTDOWN_MSG,
 };
 
+static const char * const drv_names_waipio[] = {
+	"TZ", "HYP", "HLOS", "L3", "SECPROC", "AUDIO", "SENSOR", "AOP",
+	"DEBUG", "GPU", "DISPLAY", "COMPUTE_DSP", "TME_SW", "TME_HW",
+	"MDM SW", "MDM HW", "WLAN RF", "WLAN BB", "DDR AUX", "ARC CPRF",
+	""
+};
+
 static const char * const drv_names_kalama[][MAX_DRV_NAMES] = {
 	[CXPC_DRV_NAME] = {"TZ", "HYP", "HLOS", "L3", "SECPROC", "AUDIO", "AOP", "DEBUG",
 			"GPU", "DISPLAY", "COMPUTE_DSP", "TME_SW", "TME_HW", "MDM SW",
@@ -563,6 +570,8 @@ static void vx_create_debug_nodes(struct dentry *root, struct vx_platform_data *
 }
 
 static const struct of_device_id drv_match_table[] = {
+	{ .compatible = "qcom,sys-pm-waipio",
+	  .data = drv_names_waipio },
 	{ .compatible = "qcom,sys-pm-kalama",
 	  .data = drv_names_kalama },
 	{ .compatible = "qcom,sys-pm-pineapple",